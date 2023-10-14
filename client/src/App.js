41556af7663b121341f19382f02e5717b59a8c09
Mudassar <mudassarsabirali@gmail.com>
2023-10-14 08:36:09 +0500

    Added toastify for displaying messages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Router from "./Route/Router";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Router />
+    <div className="App">
+      <Router />
+      <ToastContainer />
+    </div>
   );
 }
 
