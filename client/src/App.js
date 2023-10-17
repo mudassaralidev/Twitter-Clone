b9eb63d56b0d8a81100bbd071429fefa0653c07b
Mudassar <mudassarsabirali@gmail.com>
2023-10-17 19:07:24 +0500

    Added redux and toolkit for managing state

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,16 @@ import Router from "./Route/Router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
 function App() {
   return (
     <div className="App">
-      <Router />
-      <ToastContainer />
+      <Provider store={store}>
+        <Router />
+        <ToastContainer />
+      </Provider>
     </div>
   );
 }
